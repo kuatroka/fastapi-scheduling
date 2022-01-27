cffca1c2d2fa966ab893efb4b1a9deed55546e2d
kuatroka <aaarmani_2000@yahoo.com>
2022-01-27 14:22:37 +0000

    .

--- app/crud.py
+++ app/crud.py
@@ -1,10 +1,30 @@
+import uuid
 from cassandra.cqlengine.management import sync_table
-from .models import Product
+from .models import Product, ProductScrapeEvent
 from .db import get_session
 
 session = get_session
 sync_table(Product)
+sync_table(ProductScrapeEvent)
 
 
 def create_entry(data: dict):
     return Product.create(**data)
+
+
+def create_scrape_entry(data: dict):
+    data["uuid"] = uuid.uuid1()  # includes timestamp
+    return ProductScrapeEvent.create(**data)
+
+
+def add_scrape_event(data: dict):
+    product = create_entry(data)
+    scrape_obj = create_scrape_entry(data)
+
+    return product, scrape_obj
+
+
+data = {"asin": "AMZNIDNUMBER", "title": "Mark 1"}
+
+# ProductScrapeEvent = models.ProductScrapeEvent()
+create_scrape_entry(data)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from operator import index
 from cassandra.cqlengine import columns
 from cassandra.cqlengine.models import Model
 
@@ -7,5 +8,21 @@ from cassandra.cqlengine.models import Model
 class Product(Model):
     __keyspace__ = "scraper_app"
     asin = columns.Text(primary_key=True, required=True)
+    title = columns.Text()
+    price_str = columns.Text(default="-1")
+
+
+class ProductScrapeEvent(Model):  # -> table
+    __keyspace__ = "scraper_app"
+    uuid = columns.UUID(primary_key=True)
+    asin = columns.Text(index=True)
+    title = columns.Text()
+    price_str = columns.Text(default="-1")
+
+
+class ProductScrapeEvent(Model):  # -> table
+    __keyspace__ = "scraper_app"
+    uuid = columns.UUID(primary_key=True)
+    asin = columns.Text(index=True)
     title = columns.Text()
     price_str = columns.Text(default="-1")
